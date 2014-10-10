03dc81dbf2b40821e7af9c29e40fd048ed1812c7
Christopher Jenkins <chris.mark.jenkins@gmail.com>
2014-10-10 19:26:30 +0100

    fixed haproxy

--- haproxy/bin/boot.sh
+++ haproxy/bin/boot.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Fail hard and fast
 set -eo pipefail
@@ -6,24 +6,24 @@ set -eo pipefail
 export PUBLIC_IPV4=${PUBLIC_IPV4}
 export PRIVATE_IPV4=${PRIVATE_IPV4}
 
-readonly ETCD_PORT=${ETCD_PORT:-4001}
-readonly ETCD=$PUBLIC_IPV4:$ETCD_PORT
+export ETCD_PORT=${ETCD_PORT:-4001}
+export ETCD=$PUBLIC_IPV4:$ETCD_PORT
 
-readonly HAPROXY_PID="/var/run/haproxy.pid"
-readonly HAPROXY_CFG="/etc/haproxy/haproxy.cfg"
+export HAPROXY="/etc/haproxy"
 
 echo "[haproxy] booting container. ETCD: $ETCD"
 
-start_haproxy() {
-  haproxy -f $HAPROXY_CFG -db &
-  local pid=$!
-  echo "$pid" > $HAPROXY_PID
-}
+# Loop until confd has updated the nginx config
+until confd -verbose -onetime -node $ETCD -config-file /etc/confd/conf.d/haproxy.toml; do
+  echo "[nginx] waiting for confd to refresh haproxy.cfg"
+  sleep 5
+done
 
-start() {
-  start_haproxy
-  confd -verbose -interval 10 -node $ETCD -config-file /etc/confd/conf.d/haproxy.toml
-  echo "[haproxy] confd is listening for changes on etcd..."
-}
+# Run confd in the background to watch the upstream servers
+confd -verbose -interval 10 -node $ETCD -config-file /etc/confd/conf.d/haproxy.toml &
+echo "[haproxy] confd is listening for changes on etcd..."
 
-start
+cd "$HAPROXY"
+
+echo "[haproxy] starting haproxy agent..."
+haproxy -f /etc/haproxy/haproxy.cfg -p "/var/run/haproxy.pid" -sf $(cat /var/run/haproxy.pid)
